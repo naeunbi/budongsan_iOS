4b0ad7b628e611c7382ca06352e4058f75915944
나은비 <na-eunbi@na-eunbiui-MacBookPro.local>
2022-07-10 20:50:50 +0900

    project 생성

---
+++ budongsan app/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  budongsan app
+//
+//  Created by 나은비 on 2022/07/06.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ budongsan app/RealEstateViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RealEstateViewController.swift
+//  budongsan app
+//
+//  Created by 나은비 on 2022/07/10.
+//
+
+import UIKit
+
+class RealEstateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
