99190c9a393168e26190ae8d77b5835ced82f553
나은비 <na-eunbi@na-eunbiui-MacBookPro.local>
2022-07-10 21:39:11 +0900

    [Feat] 그림자 설정 추가

--- budongsan app/RealEstateViewController.swift
+++ budongsan app/RealEstateViewController.swift
@@ -16,6 +16,7 @@ class RealEstateViewController: UIViewController {
         super.viewDidLoad()
 
         self.configureRadius()
+        self.configureShadow()
     }
 }
 
@@ -23,8 +24,13 @@ extension RealEstateViewController {
     private func configureRadius() {
         self.earnedIncomeTaxInfo.clipsToBounds = true // 끝을 짜르겠다
         self.earnedIncomeTaxInfo.layer.cornerRadius = 12.5
-        self.earnedIncomeTax.clipsToBounds = true // 끝을 짜르겠다
         self.earnedIncomeTax.layer.cornerRadius = 21 // 네 모서리 radius 값을 21로 설정
-        
+    }
+    
+    private func configureShadow() {
+        self.earnedIncomeTax.layer.shadowColor = UIColor.lightGray.cgColor
+        self.earnedIncomeTax.layer.shadowOpacity = 0.6
+        self.earnedIncomeTax.layer.shadowOffset = .zero
+        self.earnedIncomeTax.layer.shadowRadius = 8
     }
 }
