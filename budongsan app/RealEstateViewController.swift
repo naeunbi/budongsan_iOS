39e62f69c660ca159cb77ed474d2b8f4997bcac4
나은비 <na-eunbi@na-eunbiui-MacBookPro.local>
2022-07-10 21:34:24 +0900

    [Feat] 근로소득세 UI 반영

--- budongsan app/RealEstateViewController.swift
+++ budongsan app/RealEstateViewController.swift
@@ -9,21 +9,22 @@ import UIKit
 
 class RealEstateViewController: UIViewController {
 
+    @IBOutlet weak var earnedIncomeTaxInfo: UILabel!
+    @IBOutlet weak var earnedIncomeTax: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.configureRadius()
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension RealEstateViewController {
+    private func configureRadius() {
+        self.earnedIncomeTaxInfo.clipsToBounds = true // 끝을 짜르겠다
+        self.earnedIncomeTaxInfo.layer.cornerRadius = 12.5
+        self.earnedIncomeTax.clipsToBounds = true // 끝을 짜르겠다
+        self.earnedIncomeTax.layer.cornerRadius = 21 // 네 모서리 radius 값을 21로 설정
+        
     }
-    */
-
 }
